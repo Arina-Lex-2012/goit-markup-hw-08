2d6f4b2711e40b76182ca37f94508327cc3c2dbb
Maryna Berezhanska <arina.lex2012@gmail.com>
2021-01-23 21:05:07 +0200

    menu.js

--- js/modal.js
+++ js/modal.js
@@ -1,22 +1,14 @@
 (() => {
- 
-  // const refs = {
-  //   openModalBtn: document.querySelector('[data-modal-open]'),
-  //   closeModalBtn: document.querySelector('[data-modal-close]'),
-  //   modal: document.querySelector('[data-modal]'),
-  // };
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
-  //   refs.openModalBtn.addEventListener('click', toggleModal);
-  //   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  //   function toggleModal() {
-  //     refs.modal.classList.toggle('is-hidden');
-  //   };
-
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-
-    menuBtnRef.addEventListener('click', () => {
-      menuBtnRef.classList.toggle('is-open');
-    });
-
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
 })();
